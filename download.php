aca7c092d8bb04eadfbbff2c10362c0399d76946
SeniryLove <92634652+SeniryLove@users.noreply.github.com>
2022-04-30 21:19:51 +0800

    Fix LFI Problem

--- download.php
+++ download.php
@@ -7,15 +7,25 @@ else{
 
 if(isset($_GET['fileName']) && $_GET['fileName'] == NULL){
 	header('Location: MessageBoard.php');
-}
+}else{
 
 $fileName = $_GET['fileName'];
-preg_match('/\/[^\/]+$/i',$out);
+preg_match('/\/[^\/]+$/i',$fileName,$out);
 
 if(count($out)){
 	$fileName = substr($out[0],1,strlen($out[0]) - 1);
-}
-
+	$fileName  = preg_replace('/[^\w\s\.\[\](){}\-_]+/i','',$fileName);
+	if(!file_exists("./messageFile/".($fileName))){	
+	header("Location: MessageBoard.php");
+	}else{
+	header("Content-type: ".filetype("./messageFile/".strval($fileName)));//指定類型
+	$base = preg_replace('/^[^_]+_/i', '', $fileName);
+	header("Content-Disposition: attachment; filename=".strval($base));
+	readfile("./messageFile/".$fileName);
+	}
+}	
+else{
+$fileName  = preg_replace('/[^\w\s\.\[\](){}\-_]+/i','',$fileName);
 
 if(!file_exists("./messageFile/".($fileName))){	
 	header("Location: MessageBoard.php");
@@ -24,6 +34,10 @@ if(!file_exists("./messageFile/".($fileName))){
 	$base = preg_replace('/^[^_]+_/', '', $fileName);
 	header("Content-Disposition: attachment; filename=".strval($base));
 	readfile("./messageFile/".$fileName);
+}
+	
+
+}
 }
 }
 
